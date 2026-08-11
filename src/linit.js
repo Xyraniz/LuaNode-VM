31e7bd0d8e3f5b9d6344d7ca38c83cc0ef96b101
Xyraniz <109302452+Xyraniz@users.noreply.github.com>
2026-08-11 03:54:01 -0600

    Update require paths to use stdlib directory

--- src/linit.js
+++ src/linit.js
@@ -16,16 +16,16 @@ const luaL_openlibs = function(L) {
 };
 module.exports.luaL_openlibs = luaL_openlibs;
 
-const lualib = require('./lualib.js');
-const { luaopen_base }      = require('./lbaselib.js');
-const { luaopen_coroutine } = require('./lcorolib.js');
-const { luaopen_debug }     = require('./ldblib.js');
-const { luaopen_math }      = require('./lmathlib.js');
-const { luaopen_package }   = require('./loadlib.js');
-const { luaopen_os }        = require('./loslib.js');
-const { luaopen_string }    = require('./lstrlib.js');
-const { luaopen_table }     = require('./ltablib.js');
-const { luaopen_utf8 }      = require('./lutf8lib.js');
+const lualib = require('./stdlib/lualib.js');
+const { luaopen_base }      = require('./stdlib/lbaselib.js');
+const { luaopen_coroutine } = require('./stdlib/lcorolib.js');
+const { luaopen_debug }     = require('./stdlib/ldblib.js');
+const { luaopen_math }      = require('./stdlib/lmathlib.js');
+const { luaopen_package }   = require('./stdlib/loadlib.js');
+const { luaopen_os }        = require('./stdlib/loslib.js');
+const { luaopen_string }    = require('./stdlib/lstrlib.js');
+const { luaopen_table }     = require('./stdlib/ltablib.js');
+const { luaopen_utf8 }      = require('./stdlib/lutf8lib.js');
 
 loadedlibs["_G"] = luaopen_base,
 loadedlibs[lualib.LUA_LOADLIBNAME] = luaopen_package;
@@ -37,7 +37,7 @@ loadedlibs[lualib.LUA_MATHLIBNAME] = luaopen_math;
 loadedlibs[lualib.LUA_UTF8LIBNAME] = luaopen_utf8;
 loadedlibs[lualib.LUA_DBLIBNAME] = luaopen_debug;
 if (typeof process !== "undefined")
-    loadedlibs[lualib.LUA_IOLIBNAME] = require('./liolib.js').luaopen_io;
+    loadedlibs[lualib.LUA_IOLIBNAME] = require('./stdlib/liolib.js').luaopen_io;
 
 /* Extension: fengari library */
 const { luaopen_fengari } = require('./fengarilib.js');
